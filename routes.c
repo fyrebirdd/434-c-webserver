3761f35addcba457cc3b120ca3f9c660ae38fa2c
KalebC0t <krcottyn@wcgwave.ca>
2023-04-02 21:47:42 -0600

    routes

---
+++ routes.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+struct Route{
+
+    char* path;
+    char* file;
+
+    struct Route *next;
+};
+
+struct Route* init(char* path, char* file){
+    struct Route* start = (struct Route*) malloc(sizeof(struct Route));
+
+    start->path = path;
+    start->file = file;
+
+    start->next = NULL;
+
+    return start;
+}
+
+
+struct Route* add(struct Route* r, char* path, char* file){
+    if(strcmp(path, r->path) == 0){
+        printf("ERROR: this tree already containts %s\n", path)
+    }else if (r->next == NULL)
+    {
+        r->next = init(path, file);
+    }else{
+        add(r->next, path, file);
+    }  
+}
